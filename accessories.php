7fa1f8f2d158176436a4d15cc32773d24a2f3cbe
sharaf777 <sharafabdoollah@gmail.com>
2022-08-15 12:42:30 +0400

    Initial commit

---
+++ accessories.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<head>
+<title>NEXTGEN GAMING</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="menu">
+		<div class="leftmenu">
+		<img src="logo.jpg" width="300" height="100">
+		</div>
+		<a href="#" class="toggle-button">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span></a>
+		<div class="rightmenu">
+			<ul>
+				<li><a href="index.html">HOME</a></li>
+				<li><a href="games.html">GAMES</a></li>
+				<li><a href="accessories.html">ACCESSORIES</a></li>
+				<li><a href="contacts.html">CONTACTS</a></li>
+				
+			</ul>
+		</div>
+	</div>
+<div class="containeracc">
+	<div class="card">
+		<div class="imagebox">
+			<img src="img1.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>PS5</h2>
+		<p>The PS5 is a technically impressive piece of hardware.
+		There's the new custom RDNA 2 GPU that can push 4K resolution at 120 frames per second,
+		and the octa-core AMD Zen 2-based CPU with a 3.5GHz clock speed.D</p>
+		</div>
+	</div>
+		<div class="card">
+		<div class="imagebox">
+			<img src="img2.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>PS4</h2>
+		<p>The PS5 is a technically impressive piece of hardware.
+		There's the new custom RDNA 2 GPU that can push 4K resolution at 120 frames per second,
+		and the octa-core AMD Zen 2-based CPU with a 3.5GHz clock speed.D</p>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="imagebox">
+			<img src="img3.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>XBOX One X</h2>
+		<p>The Xbox One X is the most powerful games console you can currently buy. It represents a major 
+		performance boost over the Xbox One S and is geared towards people who want to see their games at
+		the best quality possible on console.</p>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="imagebox">
+			<img src="img4.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>Nintendo Switch</h2>
+		<p>The Switch has become the console that gives you great games wherever you are, whatever the time
+		and whomever it is you want to play with. You might not want it as your primary gaming system, but 
+		you’ll definitely want it as your second.</p>
+		</div>
+	</div>
+	<div class="card">
+		<div class="imagebox">
+			<img src="img5.png">
+		</div>
+		<div class="contentacc">
+		<h2>PS5 Media Remote</h2>
+		<p>Conveniently navigate entertainment1 on your PlayStation®5 console with intuitive media and TV controls.
+		Launch Disney+, Netflix, Spotify and YouTube at the push of a button1  – even when the console is powered off.</p>
+		</div>
+	</div>
+	<div class="card">
+		<div class="imagebox">
+			<img src="img6.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>Pulse 3D Wireless Headset</h2>
+		<p>Enjoy a seamless, wireless experience with a headset fine-tuned for 3D Audio on PS5 consoles1. The PULSE 3D wireless headset
+		features a refined design with dual noise-cancelling microphones, USB Type-C® charging, and an array of easy-access controls.</p>
+		</div>
+	</div>
+	<div class="card">
+		<div class="imagebox">
+			<img src="img7.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>Razer Turret for Xbox One X</h2>
+		<p>Meet the world’s first wireless keyboard and mouse designed for Xbox One and Windows PCs. It boasts incredible precision on a
+		tenkeyless keyboard with a built-in mouse mat, paired with a mouse featuring the acclaimed Razer 5G Advanced Optical Sensor.</p>
+		</div>
+	</div>
+	<div class="card">
+		<div class="imagebox">
+			<img src="img8.jpg">
+		</div>
+		<div class="contentacc">
+		<h2>Xbox Wireless Adapter for Windows 10</h2>
+		<p>With the new and improved Xbox Wireless Adapter for Windows 10, you can play your favorite PC games using any Xbox Wireless Controller.
+		Features a 66% smaller design, wireless stereo sound support, and the ability to connect up to eight controllers at once.</p>
+		</div>
+	</div>	
+</div>
+
+</body>
+</html>
